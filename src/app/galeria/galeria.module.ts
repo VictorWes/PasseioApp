229d7caf870f4a8bb650cbe3b04dac4aaa0ce76b
Victor Wesley <victor.infowb@gmail.com>
2026-01-03 11:31:37 -0300

    feat: Adiciona modulo de galeria, e adiciona também a lógica de visualização dos lugares e suas determinadas avaliações

--- src/app/galeria/galeria.module.ts
+++ src/app/galeria/galeria.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GaleriaRoutingModule } from './galeria-routing.module';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GaleriaComponent
+  ],
   imports: [
     CommonModule,
     GaleriaRoutingModule
