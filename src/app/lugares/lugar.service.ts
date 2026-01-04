9b04a823b1baffefabae614fcdb533cf41bb0a74
Victor Wesley <victor.infowb@gmail.com>
2026-01-04 17:30:27 -0300

    feat: Adiciona lógica para ambientes de desenvolvimento,produção

--- src/app/lugares/lugar.service.ts
+++ src/app/lugares/lugar.service.ts
@@ -2,36 +2,37 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Lugar } from './lugar';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LugarService {
+  apiUrl: string = environment.apiUrl + '/lugares';
+
   constructor(private http: HttpClient) {}
 
   salvar(lugar: Lugar): Observable<Lugar> {
-    return this.http.post<Lugar>('http://localhost:3000/lugares', lugar);
+    return this.http.post<Lugar>(this.apiUrl, lugar);
   }
 
   obterTodos(): Observable<Lugar[]> {
-    return this.http.get<Lugar[]>('http://localhost:3000/lugares');
+    return this.http.get<Lugar[]>(this.apiUrl);
   }
 
   filtrar(nome: string, categoria: string): Observable<Lugar[]> {
-
     let parametros = new HttpParams();
 
-    if(nome){
-      parametros = parametros.set('nome_like', nome)
+    if (nome) {
+      parametros = parametros.set('nome_like', nome);
     }
 
-    if(categoria && categoria !== '-1'){
-      parametros = parametros.set('categoria', categoria)
+    if (categoria && categoria !== '-1') {
+      parametros = parametros.set('categoria', categoria);
     }
 
-
-    return this.http.get<Lugar[]>('http://localhost:3000/lugares', {
-      params: parametros
+    return this.http.get<Lugar[]>(this.apiUrl, {
+      params: parametros,
     });
   }
 }
